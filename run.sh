9953b8ab8b3092c750d920bf4bd5362610e126d9
root <root@beaglebone.localdomain>
2016-07-24 17:40:24 +0000

    Using lame instead of ffmpeg

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cd /sound;
-ffmpeg -ac 1 -f alsa -i hw:1,0 -acodec libmp3lame -f mp3 pipe:1 | mp3splt -f -t 30.0 m-;
+# ffmpeg -ac 1 -f alsa -i hw:1,0 -acodec libmp3lame -f mp3 pipe:1 | mp3splt -f -t 30.0 m-;
+# ffmpeg does not support recording from dsnoop, using lame instead
+arecord -D snoop -f cd -t raw -c 1 | lame -x -r - | mp3splt -f -t 30.0 m-
